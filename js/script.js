5ce9dc2485a3f19e8f118404bcd9f3d91d6ecc7a
Soma <somapy292@gmail.com>
2022-01-12 23:43:46 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Industrial Designer","Mechanical Engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
